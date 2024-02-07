3aba29f8643931e7618c7620b66da5b442c3153b
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2024-02-07 11:43:20 +0200

    Update Modal.jsx

--- src/components/Gallery/Modal/Modal.jsx
+++ src/components/Gallery/Modal/Modal.jsx
@@ -16,7 +16,7 @@ const Modal =({close , children}) => {
   useEffect(()=>{
     document.addEventListener("keydown" , closeModal);
     return ()=>document.removeEventListener('keydown',closeModal);
-  },[])
+  })
 
 
  
